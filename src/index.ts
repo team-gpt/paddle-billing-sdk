0cc7cf3c1689a7a1697d819bb335eb366b0948d8
Ilko Kacharov <kachar136@gmail.com>
2023-10-06 15:31:13 +0300

    Export webhook events

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,5 @@ export type * from './endpoints/PricingEndpoint'
 export type * from './endpoints/ProductEndpoint'
 export type * from './endpoints/SubscriptionEndpoint'
 
-import { PaddleClient } from './paddleClient'
-
-export { PaddleClient }
+export { PaddleClient } from './paddleClient'
+export { WebhookEvents, signatureHeader } from './webhooks/WebhookEvents'
